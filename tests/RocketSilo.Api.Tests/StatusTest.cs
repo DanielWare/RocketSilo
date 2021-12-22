572a5ed370819febbd52fdaa46b63b5a348e95ab
Matt Schneeberger <helluvamatt@gmail.com>
2021-12-22 17:43:38 -0600

    Add status endpoint test

---
+++ tests/RocketSilo.Api.Tests/StatusTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using RocketSilo.Api.Game;
+
+namespace RocketSilo.Api.Tests;
+
+public class StatusTest
+{
+    private IClient client = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        HttpClient httpClient = new();
+        IOptions<RocketSiloConfig> config = new OptionsWrapper<RocketSiloConfig>(new RocketSiloConfig { BaseUrl = RocketSiloConfig.DefaultBaseUrl });
+        IClientFactory clientFactory = new ClientFactory(httpClient, config);
+        client = clientFactory.GetClient(string.Empty);
+    }
+
+    [Test]
+    public async Task Test_Status()
+    {
+        GetGameStatusResponse response = await client.GetGameStatusAsync(new GetGameStatusRequest());
+        response.IsSuccess.Should().BeTrue();
+        response.Error.Should().BeNull();
+        response.Status.Should().NotBeEmpty();
+    }
+}
